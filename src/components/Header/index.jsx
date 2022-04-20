885712a465e094f1fff6300c8cadbe21a0980fd5
batya-b <batya.bicha@gmail.com>
2022-04-21 01:37:43 +0300

    create react app

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Cart from '../Cart';
+
+const Header = () => {
+    return (
+        <header>
+            <Cart />
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/components/Main/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from '../Card';
+
+const Main = () => {
+    return (
+        <main>
+            <Card />
+        </main>
+    );
+};
+
+export default Main;
