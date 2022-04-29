496c14185f5a330dc52cf8f29a4f01a22220dd61
batya-b <batya.bicha@gmail.com>
2022-04-29 13:02:38 +0300

    structure creation

--- client/src/components/Card/index.jsx
+++ client/src/components/Card/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import { React, useState } from 'react';
 import styles from './Card.module.scss';
 
 const Card = () => {
+    const { items, setItems } = useState([]);
+
     return (
         <div className={styles.card}>
             <img className={styles.cardImage} src='./img/product.png' alt='{CHANGE}' />
@@ -9,6 +11,9 @@ const Card = () => {
                 <span className={styles.cardName}>{'{Apollo Running Short}'}</span>
                 <span className={styles.cardCost}>{'{$50.00}'}</span>
             </div>
+            {items && items.map(item => (
+                console.log(item)
+            ))}
         </div>
     );
 };
